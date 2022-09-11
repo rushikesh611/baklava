9aabf0f02489d7b3378b5d1eb95a1921eeee6efd
Hrushikesh Jadhav <rushi611@gmail.com>
2022-09-11 19:34:02 +0530

    Ticket UI Box

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ import {
 } from "@thirdweb-dev/react";
 import type { NextPage } from "next";
 import Head from "next/head";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Header from "../components/Header";
 import Loading from "../components/Loading";
 import Login from "../components/Login";
@@ -17,13 +17,14 @@ import toast from "react-hot-toast";
 
 const Home: NextPage = () => {
   const address = useAddress();
+  const [userTickets, setUserTickets] = useState(0);
   const [quantity, setQuantity] = useState<number>(1);
 
   const { contract, isLoading } = useContract(
     process.env.LOTTERY_CONTRACT_ADDRESS
   );
 
-  console.log(contract);
+  // console.log(contract);
 
   const { data: expiration } = useContractData(contract, "expiration");
   const { data: remainingTickets } = useContractData(
@@ -42,6 +43,17 @@ const Home: NextPage = () => {
   );
 
   const { mutateAsync: BuyTickets } = useContractCall(contract, "BuyTickets");
+  const { data: tickets } = useContractData(contract, "getTickets");
+
+  useEffect(() => {
+    if (!tickets) return;
+    const totalTickets: string[] = tickets;
+    const noOfUserTickets = totalTickets.reduce(
+      (total, ticketAddress) => (ticketAddress === address ? total + 1 : total),
+      0
+    );
+    setUserTickets(noOfUserTickets);
+  }, [tickets, address]);
 
   const handleClick = async () => {
     if (!ticketPrice) return;
@@ -151,15 +163,49 @@ const Home: NextPage = () => {
                   expiration?.toString() < Date.now().toString() ||
                   remainingTickets?.toNumber() === 0
                 }
-                className="mt-5 w-full bg-gradient-to-br from-orange-500 to-emerald-600 px-10 py-5 rounded-md text-white shadow-xl disabled:from-gray-600 disabled:to-gray-600 disabled:cursor-not-allowed disabled:text-gray-100"
+                className="mt-5 w-full bg-gradient-to-br from-orange-500 to-emerald-600 px-10 py-5 rounded-md font-semibold text-white shadow-xl disabled:from-gray-600 disabled:to-gray-600 disabled:cursor-not-allowed disabled:text-gray-100"
               >
-                Buy Tickets
+                Buy {quantity} tickets for{" "}
+                {ticketPrice &&
+                  Number(ethers.utils.formatEther(ticketPrice.toString())) *
+                    quantity}{" "}
+                {currency}
               </button>
             </div>
+            {userTickets > 0 && (
+              <div className="stats">
+                <p className="text-lg mb-2">
+                  You have {userTickets} Tickets in this draw
+                </p>
+                <div className="flex max-w-sm flex-wrap gap-x-2 gap-y-2">
+                  {Array(userTickets)
+                    .fill("")
+                    .map((_, index) => (
+                      <p
+                        key={index}
+                        className="text-emerald-300 h-20 w-12 bg-emerald-500/30 rounded-lg flex flex-shrink-0 items-center justify-center text-xs italic"
+                      >
+                        {index + 1}
+                      </p>
+                    ))}
+                </div>
+              </div>
+            )}
           </div>
         </div>
-        <div></div>
       </div>
+      <footer className="border-t border-emerald-500/20 flex items-center text-white p-5">
+        <img
+          className="h-10 w-10 filter hue-rotate-90 opacity-20 rounded-full"
+          src="https://i.imgur.com/S2yaOd5.jpg"
+          alt=""
+        />
+        <p className="text-xs text-emerald-900 pl-5">
+          DISCLAIMER: This is a demo application and is not intended for use in
+          a production environment. The code is open source and can be found{" "}
+          <a href="https://github.com/rushikesh611/baklava">here</a>.
+        </p>
+      </footer>
     </div>
   );
 };
